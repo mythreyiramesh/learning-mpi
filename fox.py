47ccb64eac90291f683366d8c0ecafb03db22b5b
mythreyiramesh <r.mythreyi.rr@gmail.com>
2019-03-23 00:20:06 +0530

    B sent successfully

--- fox.py
+++ fox.py
@@ -69,13 +69,13 @@ if (rank != 0):
 
 # There will be same number of blocks in a dimension as the number of processors for that dimension. Hence, the block length will determine the start and end indices of the blocks along j.
 if (rank == 0):
-    print(A)
+    # print(A)
     for i in range(0,jProcs): # is it iProcs or jProcs?
         for j in range(0,iProcs):
             proc_id = i*(iProcs)+j
             if proc_id == 0:
                 continue;
-            print("Sending to",proc_id)
+            # print("Sending to",proc_id)
             rank_tag = 100 + proc_id # this is arbitrary
             start_index = (i)*block_size[0]
             end_index = C_size[0]
@@ -83,10 +83,44 @@ if (rank == 0):
                 end_index = (i+1)*block_size[0]
 
             world.Send([A[start_index:end_index][:],MPI.DOUBLE],dest=proc_id, tag=rank_tag)
-            print('Sent',start_index,'to',end_index,'towards rank',proc_id)
+            # print('Sent A',start_index,'to',end_index,'towards rank',proc_id)
 
 if (rank != 0):
     world.Recv([A_local_row,MPI.DOUBLE],source=0,tag=(100+rank))
-    print(rank,'received',A_local_row,'from',0)
+    # print(rank,'received A',A_local_row,'from',0)
+
+# Sending Array B to the processors
+# First initialise a local block B for each processor
+if (rank != 0):
+    B_local_block = np.zeros((block_size[0],block_size[1]),dtype='d')
+
+# this can be combined into the other if condition, but keeping it here for clarity
+
+if (rank == 0):
+    print(B)
+    # print(np.shape(B))
+    for i in range(0,jProcs): # is it iProcs or jProcs?
+        for j in range(0,iProcs):
+            proc_id = i*(iProcs)+j
+            if proc_id == 0:
+                continue;
+            print("Sending to",proc_id)
+            rank_tag = 200 + proc_id # this is arbitrary
+            # doubt : since the start index and end index along the j dimension (therefore i) is always fixed as 0 and block_size[j], maybe it's just best to initialise as that or send B only from 0 to block_size[0]. NO NO BIG NO
+            i_start_index = (i)*block_size[0]
+            i_end_index = C_size[0]
+            j_start_index = (j)*block_size[1]
+            j_end_index = C_size[1]
+            if (i != iProcs-1):
+                i_end_index = (i+1)*block_size[0]
+            if (j != jProcs-1):
+                j_end_index = (j+1)*block_size[1]
+            # print B[np.ix_([i_start_index,i_end_index-1],[j_start_index,j_end_index-1])]
+            world.Send([B[np.ix_([i_start_index,i_end_index-1],[j_start_index,j_end_index-1])],MPI.DOUBLE],dest=proc_id, tag=rank_tag)
+            print('Sent B',i_start_index,'to',i_end_index,'and',j_start_index,'to',j_end_index,'towards rank',proc_id)
+
+if (rank != 0):
+    world.Recv([B_local_block,MPI.DOUBLE],source=0,tag=(200+rank))
+    print(rank,'received B',B_local_block,'from',0)
 
 print("Done",rank)
